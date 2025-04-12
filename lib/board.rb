186188131e50756cff71fc5f42497aad16ee466a
Austin Vanloon <wic404learn@gmail.com>
2025-04-11 21:07:14 -0600

    Created and passed tests for cell validation

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,4 @@
-require './lib/cell'
+
 
 class Board
     attr_reader :cells
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,12 +1,21 @@
 require 'spec_helper'
-require './lib/cell'
 
 RSpec.describe Board do
-    binding.pry
-    describe '#___' do
+    #binding.pry
+    describe '#can keep track of cells and validate coordinates' do
         it 'exists' do
             board = Board.new
             expect(board).to be_a(Board)
+        end
+        it 'is a hash with 16 value pairs' do
+            board = Board.new
+            expect(board.cells.length).to eq(16)
+            expect(board.cells).to be_a(Hash)
+        end
+        it 'the key points to cell objects' do
+            board = Board.new
+            expect(board.cells.values.to_s["A1"]).to eq("A1")
+            expect(board.cells["A1"]).to be_a(Cell)
         end
     end
 end
