7878effe5820e4668d439911f3ba1ce860050ced
Nick Rathbone <nrathbone@joinhandshake.com>
2025-04-13 15:03:01 -0600

    Create additional tests for expected game class behavior and attempt to create methods to respond conditionally based on what the user inputs after starting

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,12 @@
 class Game
 
+  # using placeholders until Austin confirms how he's initializing players
+  # and differentiating between human and computer
+  attr_reader :player, :opponent
+
   def initialize
-    puts welcome_message
-    # setup_players
+   @player = Player.new
+   @opponent = Player.new(is_computer? == true)
   end
 
   def welcome_message
@@ -10,6 +14,49 @@ class Game
     "Enter 'p' to play. Enter 'q' to quit."
   end
   
+  def start_game
+    puts welcome_message
+    user_input = gets.chomp.downcase
+
+    if user_input == 'p'
+      setup_game
+    elsif user_input == 'q'
+      quit_game
+    else
+      puts "Invalid input. Please try again."
+      start_game
+    end
+
+  end
+
+  def quit_game
   
+  end
+
+  def setup_game
+    def setup_game
+      ships = [Ship.new("Cruiser", 3), Ship.new("Submarine", 2)]
+      @player.place_ships(ships)
+      @opponent.place_ships(ships)
+    end
+  end
+
+  def end_game
+    if @player.each {|ships| ships.sunk?}
+      @player.ships.all?
+      puts "You won!"
+    elsif @opponent.each {|ships| ships.sunk?}
+      @opponent.ships.all?
+      puts "I won!"
+    end
+  end
+
+  def display_boards
+    puts "=============COMPUTER BOARD============="
+    puts @opponent.board.render
+    puts "==============PLAYER BOARD=============="
+    puts @player.board.render(true)
+  end
+
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,22 +2,48 @@ require 'spec_helper'
 
 RSpec.describe Game do
 
-  # before(:each) do
-  #   @game = Game.new
-  # end
-  
-  it 'shows the player the main menu where they can play or quit' do
+  describe '#initialize' do
     game = Game.new
-    expect(game.welcome_message).to eq("Welcome to BATTLESHIP\nEnter 'p' to play. Enter 'q' to quit.")
+  
+    expect(game).to be_a(Game)
+    expect(@player).to be_a(Player)
+    expect(@opponent).to be_a(Player)
+
   end
+
+  describe '#start_game' do
+
+    before(:each) do
+      @game = Game.new
+    end
+
+    it 'shows the player the main menu where they can play or quit' do
+      expect(@game.welcome_message).to eq("Welcome to BATTLESHIP\nEnter 'p' to play. Enter 'q' to quit.")
+    end
+
+    it 'allows the user to choose to play by typing "p" has chosen to play, ' do
+      
+    end
+    
+    it 'The computer player should place their ships. The baseline computer should simply use random placements but still adhere to the valid placement rules from iteration 2.' do
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)
+    end
+    
+    it 'you need to place the computer’s ships and the players ships to set up the game.' do
+
+    end
+
+  end
+  
+  
 
   it 'Computer can place ships randomly in valid locations' do
     
   end
 
-  it 'Once the User has chosen to play, you need to place the computer’s ships and the players ships to set up the game.' do
-
-  end
+  
 
   it 'User can enter valid sequences to place both ships' do
 
@@ -27,7 +53,36 @@ RSpec.describe Game do
 
   end
 
+  it 'The game is over when either the computer or the user sinks all of the enemy ships. When this happens, the user should see a message stating who won:' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    @player.ships = [cruiser, submarine]
+
+    @player.ships.sunk?.all? == true
+    expect(end_game).to eq("You won!")
+
+    @opponent.ships = [cruiser, submarine]
+    expect(end_game).to eq("I won!")
+
+  end
+
   it 'Whenever a game ends, they should return to this message so they can start a new game, or quit.' do
 
   end
+
+  it 'At the start of the turn, the user is shown both boards. The user should see their ships but not the computer’s ships:' do
+    expected_render = 
+
+    puts "=============COMPUTER BOARD============="
+    puts @opponent.board.render
+    puts "==============PLAYER BOARD=============="
+    puts @player.board.render(true)
+
+    expect(display_boards).to eq(expected_render)
+  end
+
+
+
 end
