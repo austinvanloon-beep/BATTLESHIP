1c99cb510c371366dc3dd956cd7b1bc844944ea0
Austin Vanloon <wic404learn@gmail.com>
2025-04-13 15:59:18 -0600

    Created all_ship_sunk and computer_player methods and passing tests

--- lib/player.rb
+++ lib/player.rb
@@ -1,12 +1,34 @@
 class Player
-    attr_reader :name, :board, :ships
+    attr_reader :name, :board, :ships, :is_computer
     def initialize(name)
         @name = name
         @board = Board.new
         @ships = []
+        @is_computer = false
     end
 
     def create_ships
         cruiser = Ship.new("Cruiser", 3)
+        submarine = Ship.new("Submarine", 2)
+        @ships << cruiser
+        @ships << submarine
+    end
+
+    def place_ships
+
+    end
+
+    def take_turn(opponents_board)
+
+    end
+
+    def all_ships_sunk
+        @ships.all? {|ship| ship.sunk?}
+    end
+
+    def computer_player
+        if @name = "computer"
+            @is_computer = true
+        end
     end
 end
