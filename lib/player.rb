6ef9a32ebde6d2f13a15085cdb01ed4dbeb55d17
Nick Rathbone <nrathbone@joinhandshake.com>
2025-04-14 15:50:31 -0600

    Refactor player class file with some notes for revisiting when we add additional computer and player turn functionality

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,7 @@
 class Player
+    
     attr_reader :name, :board, :ships, :is_computer
+    
     def initialize(name)
         @name = name
         @board = Board.new
@@ -7,25 +9,47 @@ class Player
         @is_computer = false
     end
 
-    # REFACTOR create_ships so its not hardcoded for cruiser and sub
+    # placeholder for what Austin does separately
+    # note to revisit / refactor of what we have now for #create_ship_list method so that the player, when placing a ship,
+    # that doesn't necessarily always add it to the @ships array, like if it isn't a valid placement in the first place
+    # def place_ship(ship_list)
+        
+    #     ship_list.each do |ship|
+    #         if @is_computer
+    #           place_ship_randomly(ship)
+    #         else
+    #           prompt_for_ship_placement(ship)
+    #         end
 
-    def create_ships
-        cruiser = Ship.new("Cruiser", 3)
-        submarine = Ship.new("Submarine", 2)
-        @ships << cruiser
-        @ships << submarine
-    end
+    #     @ships << ship
+    #     end
+    # end
 
-    def take_turn(opponents_board)
+    # placeholder for what Austin does separately
+    # def prompt_for_ship_placement
 
-    end
+    # end
 
+    # placeholder for what Austin does separately
+    # def place_ship_randomly
+
+    # end
+
+    # placeholder for what Austin does separately
+    # def take_turn(opponents_board)
+    #     if @is_computer == true
+    #         fire_randomly(opponents_board)
+    #     else
+    #         fire_prompt(opponents_board)
+    #     end
+    # end
+    
     def all_ships_sunk
         @ships.all? {|ship| ship.sunk?}
     end
 
     def computer_player
-        if @name = "computer"
+        if @name == "computer"
             @is_computer = true
         end
     end
