08dcb18ff1da3dc0bc19d31048872b89ac6825ef
Nick Rathbone <nrathbone@joinhandshake.com>
2025-04-15 19:25:30 -0600

    refactor the cell class fire_upon logic to prevent double damage and a note for future verification of the render method

--- lib/cell.rb
+++ lib/cell.rb
@@ -20,35 +20,27 @@ class Cell
     @fired_upon
   end
 
-  # question for later -- is this like when the user/player chooses a cell to "attack?"
-  # refactor note may need to revisit this when we code the user input logic
-  def fire_upon(fire_coordinate)
-    if fire_coordinate == @coordinate
-      if empty? == true
-        @fired_upon = true
-      elsif empty? == false
-        @ship.hit
-        @fired_upon = true
-      end
-    end
+  def fire_upon(cell)
+    # added this to prevent double damage
+    return if @fired_upon
+
+    @fired_upon = true
+    @ship.hit unless empty?
   end
 
-  # refactor note, I wonder if we could use what we learned about falsey and truthy to simplify this
-  # since any object that's not 'nil' or 'false' is treated as truthy.
-  
-  # added an optional argument that isn't required when calling the method by setting a default value
+  # check this method out later to make sure it's working correctly in game logic
   def render(show_ship = false)
-    if fired_upon? == false
-      if show_ship == true && empty? == false
+    if fired_upon?
+      if show_ship && empty? == false
         return "S"
       else
         return "."
       end
-    elsif fired_upon? == true && empty? == true
+    elsif fired_upon? && empty?
       return "M"
-    elsif fired_upon? == true && empty? == false && @ship.sunk? == true
+    elsif fired_upon? && empty? == false && @ship.sunk?
       return "X"
-    elsif fired_upon? == true && empty? == false
+    elsif fired_upon? && empty? == false
       return "H"
     end
   end
